8a7238c81960314822ee0e09f8aaf7841958ca8c
Jonas Almeida <jalmeida@mathbiol.org>
2019-09-12 16:21:04 -0400

    jonas get.Iris

--- jonas.js
+++ jonas.js
@@ -4,6 +4,7 @@ jonas=function(){
     console.log('jonas fun initialized at '+Date())
 }
 
-jonas.getIris=function(){
-    
+jonas.getIris=async function(){
+    jonas.irisData=await (await fetch('https://episphere.github.io/ai/data/iris.json')).json()
+    return jonas.irisData
 }
